68f82efe2973f9d39fb9008668555635110e13f4
Tyrelle Adams <tyrelle@Tyrelles-MBP-2.attlocal.net>
2022-10-16 20:31:49 -0700

    Keeper App Part 3, added more functionality to app (add note/delete note)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
-//noteEntry function that inputs a noteItem and returns a key, title, and content from notes.js
-function noteEntry(noteItem) {
-    return (
-        <Note
-        key={noteItem.key}
-        title={noteItem.title}
-        content={noteItem.content}
-        />
-    )
-}
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
 
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
-        <div>
-            <Header />
-            {/* maps the notes array using the noteEntry function */}
-            {notes.map(noteEntry)}
-            <Footer />
-        </div>
+      <div>
+        <Header />
+        <CreateArea onAdd={addNote}/>
+        {notes.map((noteItem, index) => {
+            return (
+               <Note 
+               key={index}
+               id={index}
+               title={noteItem.title}
+               content={noteItem.content}
+               onDelete={deleteNote} /> 
+            );
+        })}
+        
+        <Footer />
+      </div>
     );
-}
+  }
 
 export default App;
